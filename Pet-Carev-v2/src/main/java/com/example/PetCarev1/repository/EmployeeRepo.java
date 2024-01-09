edc4454ed267cf2c9d83450017f3363c35b0f67e
Abdullah <abdullahelagami55@gmail.com>
2024-01-09 19:39:53 +0200

    Add findByEndTimeIsNullOrEndTimeIsBefore method

--- Pet-Carev-v2/src/main/java/com/example/PetCarev1/repository/EmployeeRepo.java
+++ Pet-Carev-v2/src/main/java/com/example/PetCarev1/repository/EmployeeRepo.java
@@ -3,10 +3,14 @@ package com.example.PetCarev1.repository;
 
 import com.example.PetCarev1.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
 
+    Optional<List<Employee>> findByEndTimeIsNullOrEndTimeIsBefore(LocalDateTime dueDate);
 }
