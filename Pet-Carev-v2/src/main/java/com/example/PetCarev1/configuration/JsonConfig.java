31eeb2e0126d4abc2de2380361446de2611f378d
Abdullah <abdullahelagami55@gmail.com>
2024-01-09 19:43:11 +0200

    Add Retrying behavior

--- Pet-Carev-v2/src/main/java/com/example/PetCarev1/configuration/JsonConfig.java
+++ Pet-Carev-v2/src/main/java/com/example/PetCarev1/configuration/JsonConfig.java
@@ -5,8 +5,10 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
 
 @Configuration
+@EnableRetry
 public class JsonConfig {
 
     @Bean
